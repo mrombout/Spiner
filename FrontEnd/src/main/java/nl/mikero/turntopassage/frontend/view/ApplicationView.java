b104d208f103a22b119672a44f2437f9e4450bee
Mike Rombout <mike.rombout1@gmail.com>
2016-10-22 14:11:45 +0200

    Update to do transform on separate thread

--- FrontEnd/src/main/java/nl/mikero/turntopassage/frontend/view/ApplicationView.java
+++ FrontEnd/src/main/java/nl/mikero/turntopassage/frontend/view/ApplicationView.java
@@ -1,7 +1,9 @@
 package nl.mikero.turntopassage.frontend.view;
 
 import com.google.inject.Inject;
+import javafx.concurrent.Worker;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,6 +14,7 @@ import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 import nl.mikero.turntopassage.core.exception.TwineRepairFailedException;
 import nl.mikero.turntopassage.core.service.TwineService;
+import nl.mikero.turntopassage.frontend.TransformTask;
 import nl.mikero.turntopassage.frontend.control.DropFileChooser;
 import nl.mikero.turntopassage.frontend.dialog.ExceptionDialog;
 import org.apache.commons.io.FilenameUtils;
@@ -20,22 +23,21 @@ import java.io.*;
 import java.util.Optional;
 
 public class ApplicationView {
-    private final TwineService twineService;
-
     private static final Image fileImage = new Image("/file.png");
     private static final Image progressImage = new Image("/progress.png");
     private static final Image doneImage = new Image("/done.png");
 
     private final Alert errorAlert;
 
+    @Inject
+    private TwineService twineService;
+
     @FXML
     private DropFileChooser dropFileChooser;
     @FXML
     private Button transformButton;
 
-    @Inject
-    public ApplicationView(TwineService twineService) {
-        this.twineService = twineService;
+    public ApplicationView() {
         this.errorAlert = new Alert(Alert.AlertType.ERROR);
     }
 
@@ -64,6 +66,10 @@ public class ApplicationView {
         });
     }
 
+    public void onAutomaticTransformStatusMouseClicked(Event event) {
+
+    }
+
     public void onTransformButtonClicked(ActionEvent actionEvent) {
         final File inputFile = dropFileChooser.getFile();
         final File outputFile = new File(getOutputPath(inputFile.getAbsolutePath()));
@@ -80,37 +86,41 @@ public class ApplicationView {
             }
         }
 
-        try(InputStream in = new BufferedInputStream(new FileInputStream(inputFile));
-            OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFile))) {
-            dropFileChooser.setImage(progressImage);
-            twineService.transform(in, out);
-            dropFileChooser.setImage(doneImage);
-        } catch (FileNotFoundException e) {
-            String title = String.format("File '%s' could not be found.", inputFile.toString());
+        dropFileChooser.setImage(progressImage);
+        TransformTask task = new TransformTask(twineService, inputFile, outputFile);
+        new Thread(task).start();
+        task.stateProperty().addListener((observable, oldState, newState) -> {
+            if(newState.equals(Worker.State.SUCCEEDED))
+                dropFileChooser.setImage(doneImage);
+        });
+        task.exceptionProperty().addListener((observable, oldException, newException) -> {
+            if(newException instanceof FileNotFoundException) {
+                String title = String.format("File '%s' could not be found.", inputFile.toString());
 
-            errorAlert.setAlertType(Alert.AlertType.CONFIRMATION);
-            errorAlert.setTitle(title);
-            errorAlert.setHeaderText(title);
-            errorAlert.setContentText(title + " Do you want to select a different file and try again?");
-            Optional<ButtonType> result = errorAlert.showAndWait();
-            if(result.isPresent() && result.get() == ButtonType.OK) {
-                dropFileChooser.openFileChooser();
-            }
-        } catch (TwineRepairFailedException e) {
-            String title = String.format("File '%s' could not be repaired.", inputFile.toString());
+                errorAlert.setAlertType(Alert.AlertType.CONFIRMATION);
+                errorAlert.setTitle(title);
+                errorAlert.setHeaderText(title);
+                errorAlert.setContentText(title + " Do you want to select a different file and try again?");
+                Optional<ButtonType> result = errorAlert.showAndWait();
+                if(result.isPresent() && result.get() == ButtonType.OK) {
+                    dropFileChooser.openFileChooser();
+                }
+            } else if(newException instanceof TwineRepairFailedException) {
+                String title = String.format("File '%s' could not be repaired.", inputFile.toString());
 
-            errorAlert.setAlertType(Alert.AlertType.ERROR);
-            errorAlert.setTitle(title);
-            errorAlert.setHeaderText(title);
-            errorAlert.setContentText(title + " The file might be in a format that TurnToPassage does not understand. Do you want to select a different file and try again?");
-            Optional<ButtonType> result = errorAlert.showAndWait();
-            if(result.isPresent() && result.get() == ButtonType.OK) {
-                dropFileChooser.openFileChooser();
+                errorAlert.setAlertType(Alert.AlertType.ERROR);
+                errorAlert.setTitle(title);
+                errorAlert.setHeaderText(title);
+                errorAlert.setContentText(title + " The file might be in a format that TurnToPassage does not understand. Do you want to select a different file and try again?");
+                Optional<ButtonType> result = errorAlert.showAndWait();
+                if(result.isPresent() && result.get() == ButtonType.OK) {
+                    dropFileChooser.openFileChooser();
+                }
+            } else if(newException instanceof IOException) {
+                ExceptionDialog exceptionDialog = new ExceptionDialog(newException);
+                exceptionDialog.showAndWait();
             }
-        } catch (IOException e) {
-            ExceptionDialog exceptionDialog = new ExceptionDialog(e);
-            exceptionDialog.showAndWait();
-        }
+        });
     }
 
     private String getOutputPath(String path) {
