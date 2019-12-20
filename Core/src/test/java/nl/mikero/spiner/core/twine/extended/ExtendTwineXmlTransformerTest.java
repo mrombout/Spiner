495d67c5503457824475a0ecedabf4d2e4093de0
Mike Rombout <mike.rombout1@gmail.com>
2019-12-20 20:18:38 +0100

    Remove unused import org.junit.Assert.assertNotEquals

--- Core/src/test/java/nl/mikero/spiner/core/twine/extended/ExtendTwineXmlTransformerTest.java
+++ Core/src/test/java/nl/mikero/spiner/core/twine/extended/ExtendTwineXmlTransformerTest.java
@@ -25,7 +25,6 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 public class ExtendTwineXmlTransformerTest {
 
